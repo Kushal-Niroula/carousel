db5c899a442977a0021c82ac7dba6eb1b88b3fbb
Kushal <niroulakushal3@gmail.com>
2021-01-16 14:17:24 +0545

    functioning carousel

---
+++ js/index.js
@@ -0,0 +1,71 @@
+var wrapper = document.getElementsByClassName("image-wrapper")[0];
+var images = wrapper.getElementsByTagName("img");
+const imgWidth=400;
+
+const divWidth = images.length * imgWidth;
+wrapper.style.width = divWidth +"px";
+var marginLeft = 0;
+var left = document.getElementById("left");
+var right = document.getElementById("right");
+
+var circles = document.getElementsByClassName("circles")[0];
+
+for (var i=0 ;i<images.length;i++){
+    let dots =document.createElement("i");
+    dots.className = "far fa-circle";
+    dots.setAttribute("id",`${i}dot`);
+    dots.style.padding="2px";
+    circles.appendChild(dots);
+}
+var arryaDots = document.getElementsByClassName("far fa-circle");
+var flag=0;
+Array.from(arryaDots).forEach(function(dots){
+    dots.addEventListener("mouseover",function(){
+        dots.style.backgroundColor="blue";
+    })
+    dots.addEventListener("mouseleave",function(){
+        dots.style.backgroundColor="inherit"
+    
+    })
+    dots.addEventListener("click",function(){
+        let id =parseInt(dots.id);
+        dots.style.backgroundColor="red";
+        marginLeft = (-1)* id * imgWidth;
+        wrapper.style.marginLeft = marginLeft + "px";
+        checkVisibility();
+
+    })
+    
+    
+})
+
+
+
+checkVisibility();
+left.addEventListener("click",function(){
+    marginLeft=marginLeft+imgWidth;
+    wrapper.style.marginLeft= marginLeft + "px";
+    checkVisibility();
+  
+
+})
+right.addEventListener("click",function(){
+    marginLeft= marginLeft-imgWidth;
+    wrapper.style.marginLeft=marginLeft + "px";
+    checkVisibility();
+   
+})
+function checkVisibility(){
+    if(marginLeft===0){
+        left.style.visibility = "hidden";
+    }
+    else{
+        left.style.visibility = "visible";
+    }
+    if(marginLeft===(-1)*(divWidth-imgWidth)){
+        right.style.visibility = "hidden";
+    }
+    else{
+        right.style.visibility="visible";
+    }
+}
